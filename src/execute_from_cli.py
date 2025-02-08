9eaea946be8591cc0989da75164b4917da68ea29
AranParrish <153287399+AranParrish@users.noreply.github.com>
2025-02-08 21:54:57 +0000

    add doc string

--- src/execute_from_cli.py
+++ src/execute_from_cli.py
@@ -1,23 +1,30 @@
 import streaming_data, argparse
 
 
-def main():
+def execute_from_cli():
+    """Runs the streaming_data application using the argparse module to pass in arguments from the command line.
+
+    Creates an argument parser with help messages to guide the user through usage.
+    Args:
+        None.
+    Returns:
+        None, but successful execution results in the search query being stored in an AWS SQS queue.
+    """
+
     parser = argparse.ArgumentParser(
         description="Post up to 10 post recent search results from the Guardian API in an AWS SQS queue"
     )
-    parser.add_argument(
-        "search_term", help="Enter your search term as a string", type=str
-    )
+    parser.add_argument("search_term", help="Enter your search term (string)", type=str)
     parser.add_argument(
         "message_broker_id",
-        help="The ID used to identify the search results in your AWS SQS queue",
+        help="The ID used to identify the search results in your AWS SQS queue (string)",
         type=str,
     )
     parser.add_argument(
         "-d",
         "--date_from",
         required=False,
-        help="Optionally restrict search results from after this date, provided as a valid ISO 8601 format date string",
+        help="Optionally restrict search results from after this date (valid ISO 8601 format date string, such as YYYY-MM-DD)",
         type=str,
     )
     parser.add_argument(
@@ -37,4 +44,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    execute_from_cli()
